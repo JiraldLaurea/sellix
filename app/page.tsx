5d658a776336fd1e1d581a6c3e49b26b16979a3a
JiraldLaurea <jiraldlaurea@gmail.com>
2025-12-31 07:06:56 +0800

    chore: route homepage to product listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import ShopPage from "./(shop)/page";
+import Navbar from "@/components/layout/Navbar";
 
 export default function Home() {
-    return <ShopPage />;
+    return (
+        <>
+            <ShopPage />
+        </>
+    );
 }
