66416344dc53b0fd8d4772fab2255d3a201134fa
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-07 02:43:41 +0800

    fix: prismaClient error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,18 @@
 export const runtime = "nodejs";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
-
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
 
 export const prisma =
     globalForPrisma.prisma ??
