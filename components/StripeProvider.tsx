8ea91f3fa8d350221e1f9be11ae353574418ad18
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-09 22:50:34 +0800

    feat: remove strip developer tools

--- components/StripeProvider.tsx
+++ components/StripeProvider.tsx
@@ -4,7 +4,14 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
+    {
+        developerTools: {
+            assistant: {
+                enabled: false,
+            },
+        },
+    }
 );
 
 export default function StripeProvider({
