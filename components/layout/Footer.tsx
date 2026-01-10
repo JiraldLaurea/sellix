031ae303e4f3498477e7b1cc45047962e4078830
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-10 08:34:27 +0800

    style: update footer text

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -13,9 +13,7 @@ export default function Footer() {
     return (
         <div className="flex justify-center py-10 border-t bg-gray-50">
             <div className="flex flex-col-reverse items-center justify-center w-full max-w-6xl px-4 sm:justify-between sm:flex-row">
-                <p className="text-sm text-secondary-text dark:text-secondary-text-dark">
-                    ©2026 Jirald Calusay
-                </p>
+                <p className="text-sm text-gray-600">©2026 Jirald Calusay</p>
             </div>
             <div />
         </div>
