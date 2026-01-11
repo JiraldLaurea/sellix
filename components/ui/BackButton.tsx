217674b0da29d3270a04fddfd2c7ed464613256c
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-11 14:30:20 +0800

    refactor: BackButton

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -1,19 +1,18 @@
-import { useRouter } from "next/navigation";
+import { useRouter } from "nextjs-toploader/app";
 import { IoIosArrowBack } from "react-icons/io";
 
 type Props = {
     text: string;
-    hasHref?: boolean;
     href?: string;
 };
 
-export function BackButton({ text, hasHref, href }: Props) {
+export function BackButton({ text, href }: Props) {
     const router = useRouter();
 
     return (
         <button
             onClick={() => {
-                if (hasHref) router.push(`${href}`);
+                if (href) router.push(`${href}`);
                 else router.back();
             }}
             className="flex items-center h-10 py-1 pl-2 pr-4 space-x-1 text-sm transition-colors border rounded-lg cursor-pointer w-fit hover:bg-gray-100"
