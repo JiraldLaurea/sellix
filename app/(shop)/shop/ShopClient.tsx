b9b8f6e722f973339edd3d2f5d52a0eb3811a302
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-13 01:45:58 +0800

    refactor: server and client seperation

---
+++ app/(shop)/shop/ShopClient.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import ProductCard from "@/components/product/ProductCard";
+import PageContainer from "@/components/ui/PageContainer";
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+import { Product } from "../../types/product";
+
+type ShopClientProps = {
+    products: Product[];
+};
+
+export default function ShopClient({ products }: ShopClientProps) {
+    return (
+        <>
+            <PageContainer>
+                <div id="nav-trigger" className="h-px w-px" />
+                <h1 className="mb-4 text-4xl font-semibold">
+                    Products Catalog
+                </h1>
+                <div
+                    id="nav-trigger"
+                    className="grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-3 lg:grid-cols-4"
+                >
+                    {products ? (
+                        <>
+                            {products.map((product: Product) => (
+                                <ProductCard
+                                    key={product.id}
+                                    product={product}
+                                />
+                            ))}
+                        </>
+                    ) : (
+                        <>
+                            <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
+                            <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
+                            <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
+                            <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
+                        </>
+                    )}
+                </div>
+            </PageContainer>
+        </>
+    );
+}
--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -1,10 +1,8 @@
-import ProductCard from "@/components/product/ProductCard";
 import { getProducts } from "@/lib/getProducts";
-import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
-import { Product } from "../../types/product";
-import PageContainer from "@/components/ui/PageContainer";
+import ShopClient from "./ShopClient";
 
 export default async function ShopPage() {
     const session = await getServerSession(authOptions);
@@ -15,25 +13,5 @@ export default async function ShopPage() {
 
     const products = await getProducts();
 
-    return (
-        <PageContainer>
-            <h1 className="mb-4 text-4xl font-semibold">Products Catalog</h1>
-            <div className="grid grid-cols-2 gap-4 sm:gap-6 md:grid-cols-3 lg:grid-cols-4">
-                {products ? (
-                    <>
-                        {products.map((product: Product) => (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
-                    </>
-                ) : (
-                    <>
-                        <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
-                        <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
-                        <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
-                        <div className="flex flex-col bg-gray-100 border rounded-lg h-107 animate-pulse" />
-                    </>
-                )}
-            </div>
-        </PageContainer>
-    );
+    return <ShopClient products={products} />;
 }
