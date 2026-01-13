7c0e6aa1b88c1a93e27cfdb59335bd88e6644f8e
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-13 11:38:19 +0800

    style: remove unused className

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -15,7 +15,7 @@ export function BackButton({ text, href }: Props) {
                 if (href) router.push(`${href}`);
                 else router.back();
             }}
-            className="flex items-center h-10 py-1 pl-2 pr-4 space-x-1 text-sm transition-colors border rounded-lg cursor-pointer w-fit hover:bg-gray-100"
+            className="flex items-center h-10 pl-2 pr-4 space-x-1 text-sm transition-colors border rounded-lg cursor-pointer w-fit hover:bg-gray-100"
         >
             <IoIosArrowBack size={18} />
             <p>{text}</p>
