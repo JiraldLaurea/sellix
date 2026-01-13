eca2365404a3a52f5dcf25a0a84b36b91aff95f3
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-13 15:20:50 +0800

    feat: redirect unauthenticated users

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 import HeroCarousel from "./(marketing)/components/HeroCarousel";
 import ShopPage from "./(shop)/shop/page";
 
-export default function Home() {
+export default async function Home() {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect("/login");
+    }
+
     return (
         <>
             <HeroCarousel />
