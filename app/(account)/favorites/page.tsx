3daf9a2a8469f70dad43f40b2836b27ca2727c4a
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-14 03:39:19 +0800

    feat: init favorites page

---
+++ app/(account)/favorites/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function FavoritesPage() {
+    return (
+        <section className="max-w-3xl">
+            <h1 className="mb-6 text-3xl font-semibold">Favorites</h1>
+        </section>
+    );
+}
