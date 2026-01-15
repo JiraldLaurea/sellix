5b146dea68d0e0d809aa8e43d5a12b27ac921367
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-15 16:48:54 +0800

    fix: quantity picker

--- components/product/ProductPurchase.tsx
+++ components/product/ProductPurchase.tsx
@@ -5,7 +5,7 @@ import QuantityPicker from "@/components/cart/QuantityPicker";
 import AddToCartButton from "@/components/product/AddToCartButton";
 import { useCart } from "@/lib/cart-context";
 import { showWarningToast } from "@/lib/toast/showWarningToast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {
     product: Product;
@@ -18,19 +18,22 @@ export default function ProductPurchase({ product }: Props) {
     const quantityInCart = cartItem?.quantity ?? 0;
     const remainingStock = Math.max(product.stock - quantityInCart, 0);
 
-    const max = remainingStock;
+    const [quantity, setQuantity] = useState(0);
 
-    const [quantity, setQuantity] = useState(remainingStock > 0 ? 1 : 0);
+    // Keep quantity in sync with remaining stock
+    useEffect(() => {
+        setQuantity(remainingStock > 0 ? 1 : 0);
+    }, [remainingStock]);
+
     const handleQuantityChange = (next: number) => {
         if (remainingStock === 0) {
             setQuantity(0);
             showWarningToast("No more stock available");
-
             return;
         }
 
-        if (next < 1) {
-            setQuantity(1);
+        if (next < 0) {
+            setQuantity(0);
             return;
         }
 
@@ -39,21 +42,31 @@ export default function ProductPurchase({ product }: Props) {
             showWarningToast("No more stock available");
             return;
         }
+
         setQuantity(next);
     };
 
     return (
-        <div className="flex items-center space-x-4">
-            <QuantityPicker
-                quantity={quantity}
-                max={max}
-                onChange={handleQuantityChange}
-            />
-            <AddToCartButton
-                buttonType="regular"
-                product={product}
-                quantity={quantity}
-            />
+        <div>
+            <div className="flex items-center space-x-4">
+                <QuantityPicker
+                    quantity={quantity}
+                    max={remainingStock}
+                    onChange={handleQuantityChange}
+                />
+                <AddToCartButton
+                    buttonType="regular"
+                    product={product}
+                    quantity={quantity}
+                    disabled={quantity === 0} // ✅ disable when 0
+                />
+            </div>
+            {remainingStock === 0 && (
+                <p className="text-xs p-4 bg-amber-50 rounded-lg text-amber-500 mt-2 sm:text-left text-center">
+                    You have reached the maximum quantity available for this
+                    item
+                </p>
+            )}
         </div>
     );
 }
