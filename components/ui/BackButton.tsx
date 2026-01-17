6f6c6952807016028c98be970a2144226f2ba118
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-18 01:59:47 +0800

    style: update back button

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -15,10 +15,20 @@ export function BackButton({ text, href }: Props) {
                 if (href) router.push(`${href}`);
                 else router.back();
             }}
-            className="flex items-center h-10 pl-2 pr-4 space-x-1 text-sm transition-colors border rounded-lg cursor-pointer w-fit hover:bg-gray-100"
+            className="flex items-center h-10 pr-4 space-x-1 text-blue-500 transition-colors cursor-pointer hover:underline w-fit"
         >
-            <IoIosArrowBack size={18} />
+            <IoIosArrowBack size={20} />
             <p>{text}</p>
         </button>
+        // <button
+        //     onClick={() => {
+        //         if (href) router.push(`${href}`);
+        //         else router.back();
+        //     }}
+        //     className="flex items-center h-10 pl-2 pr-4 space-x-1 text-sm transition-colors border rounded-lg cursor-pointer w-fit hover:bg-gray-100"
+        // >
+        //     <IoIosArrowBack size={18} />
+        //     <p>{text}</p>
+        // </button>
     );
 }
