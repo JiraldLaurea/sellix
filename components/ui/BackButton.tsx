03a714cd20cdd5cc784d93595ac02f81d6ba6048
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-18 16:13:26 +0800

    style: update back button

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -1,5 +1,11 @@
 import { useRouter } from "nextjs-toploader/app";
+import { FaAngleLeft } from "react-icons/fa6";
 import { IoIosArrowBack } from "react-icons/io";
+import { IoArrowBack, IoChevronBack } from "react-icons/io5";
+import {
+    MdOutlineArrowBackIos,
+    MdOutlineArrowBackIosNew,
+} from "react-icons/md";
 
 type Props = {
     text: string;
@@ -15,20 +21,10 @@ export function BackButton({ text, href }: Props) {
                 if (href) router.push(`${href}`);
                 else router.back();
             }}
-            className="flex items-center h-10 pr-4 space-x-1 text-blue-500 transition-colors cursor-pointer hover:underline w-fit"
+            className="flex items-center h-10 pr-4 relative right-1.5 space-x-1 text-blue-500 transition-colors cursor-pointer hover:underline w-fit"
         >
-            <IoIosArrowBack size={20} />
+            <IoChevronBack size={20} className="" />
             <p>{text}</p>
         </button>
-        // <button
-        //     onClick={() => {
-        //         if (href) router.push(`${href}`);
-        //         else router.back();
-        //     }}
-        //     className="flex items-center h-10 pl-2 pr-4 space-x-1 text-sm transition-colors border rounded-lg cursor-pointer w-fit hover:bg-gray-100"
-        // >
-        //     <IoIosArrowBack size={18} />
-        //     <p>{text}</p>
-        // </button>
     );
 }
