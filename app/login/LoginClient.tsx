d289eaa306b88a7fbdc32142a1f027b22c81dd8b
JiraldLaurea <jiraldlaurea@gmail.com>
2026-01-26 14:38:53 +0800

    chore: update login page to include manual sign in

--- app/login/LoginClient.tsx
+++ app/login/LoginClient.tsx
@@ -3,17 +3,22 @@
 import { Button } from "@/components/ui/Button";
 import { Container } from "@/components/ui/Container";
 import PageContainer from "@/components/ui/PageContainer";
-import { Spinner } from "@/components/ui/Spinner";
 import { GitHubLogo, GoogleLogo } from "@/public/img/login/svgs";
 import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { IoIosInformationCircle } from "react-icons/io";
 
 type Provider = "google" | "github" | "credentials" | null;
 
 export default function LoginClient() {
     const { status } = useSession();
     const [loadingProvider, setLoadingProvider] = useState<Provider>(null);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const googleEnabled = process.env.NEXT_PUBLIC_GOOGLE_ENABLED === "true";
     const githubEnabled = process.env.NEXT_PUBLIC_GITHUB_ENABLED === "true";
@@ -29,7 +34,7 @@ export default function LoginClient() {
 
     return (
         <PageContainer className="py-0! max-w-full sm:px-0!">
-            <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
+            <div className="grid min-h-screen grid-cols-1 md:grid-cols-[400px_1fr] lg:grid-cols-[550px_1fr]">
                 {/* LEFT — SIDE CONTENT */}
                 <div className="flex-col items-center justify-center hidden w-full px-8 space-y-10 text-center text-white border-r md:flex bg-accent">
                     <Image
@@ -62,8 +67,8 @@ export default function LoginClient() {
                 </div>
 
                 {/* RIGHT — LOGIN */}
-                <div className="flex items-center justify-center">
-                    <Container className="w-full max-w-lg px-0 sm:p-8">
+                <div className="flex items-center w-full justify-center">
+                    <Container className="w-full max-w-lg px-0 py-6 sm:p-6">
                         <Image
                             src="/img/brand_logo_light.png"
                             alt="Brand Logo"
@@ -133,23 +138,89 @@ export default function LoginClient() {
                         </div>
 
                         {/* Demo credentials */}
-                        {/* TODO */}
-                        <Button
-                            disabled={!!loadingProvider}
-                            onClick={async () => {
-                                setLoadingProvider("credentials");
-                                await signIn("credentials", {
-                                    callbackUrl: "/",
-                                    redirect: true,
-                                });
-                            }}
+                        <div className="space-y-4">
+                            {/* Email */}
+                            <div className="focus-within:text-blue-500">
+                                <p className="text-sm font-medium mb-1">
+                                    Email address
+                                </p>
+                                <input
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="you@example.com"
+                                    className="w-full text-black leading-10 rounded-lg border px-3 h-10 focus:outline-none transition-all focus:ring-2 focus:border-transparent focus:ring-blue-500"
+                                />
+                            </div>
 
-                            // className="w-full bg-accent hover:bg-gray-800 transition text-white py-2 rounded-md disabled:opacity-60"
-                        >
-                            {loadingProvider === "credentials"
-                                ? "Signing in…"
-                                : "Sign in (demo)"}
-                        </Button>
+                            {/* Password */}
+                            <div className="focus-within:text-blue-500">
+                                <p className="text-sm font-medium mb-1">
+                                    Password
+                                </p>
+                                <div className="relative focus-within:ring-2 border focus-within:border-transparent focus-within:ring-blue-500 transition-all rounded-lg">
+                                    <input
+                                        type={
+                                            showPassword ? "text" : "password"
+                                        }
+                                        value={password}
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                        className="w-full text-black px-3 h-10 leading-10 focus:outline-none"
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() =>
+                                            setShowPassword((v) => !v)
+                                        }
+                                        className="absolute text-black -translate-y-1/2 top-1/2 right-3 "
+                                    >
+                                        {showPassword ? (
+                                            <BsEye size={18} />
+                                        ) : (
+                                            <BsEyeSlash size={18} />
+                                        )}
+                                    </button>
+                                </div>
+                            </div>
+
+                            <Button
+                                className="w-full"
+                                disabled={!!loadingProvider}
+                                onClick={async () => {
+                                    setLoadingProvider("credentials");
+                                    await signIn("credentials", {
+                                        email,
+                                        password,
+                                        callbackUrl: "/",
+                                    });
+                                }}
+                            >
+                                {loadingProvider === "credentials"
+                                    ? "Signing in…"
+                                    : "Sign in"}
+                            </Button>
+
+                            <button className="text-sm text-blue-500 hover:underline text-right w-full">
+                                Forgot password?
+                            </button>
+                        </div>
+
+                        <div className="px-4 py-3 bg-blue-50 rounded-lg mt-4 flex items-center space-x-1">
+                            <IoIosInformationCircle
+                                size={24}
+                                className="text-blue-500"
+                            />
+                            <p>
+                                Use{" "}
+                                <span className="font-semibold">
+                                    admin@email.com
+                                </span>{" "}
+                                with password{" "}
+                                <span className="font-semibold">secret12</span>
+                            </p>
+                        </div>
                     </Container>
                 </div>
             </div>
