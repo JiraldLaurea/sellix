6b929a1d7bcabc3f1e42c92c055881628bae9969
JiraldLaurea <jiraldlaurea@gmail.com>
2026-02-01 20:22:15 +0800

    refactor: unused code

--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -1,16 +1,7 @@
 import { getProducts } from "@/lib/getProducts";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../../api/auth/[...nextauth]/route";
 import ShopClient from "./ShopClient";
 
 export default async function ShopPage() {
-    const session = await getServerSession(authOptions);
-
-    if (!session) {
-        redirect("/login");
-    }
-
     const { items, nextCursor } = await getProducts();
 
     return <ShopClient initialProducts={items} initialCursor={nextCursor} />;
